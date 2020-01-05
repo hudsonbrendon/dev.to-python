6b191191d30db50d0a782c86ccd9e202e7e2924d
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2020-01-04 23:07:25 -0300

    Added method articles

--- dev/dev.py
+++ dev/dev.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class DevTo(object):
 
     _URL = 'https://dev.to/api'
@@ -7,3 +10,26 @@ class DevTo(object):
 
     def _get_url(self, path):
         return f'{self._URL}{path}'
+
+    def articles(self, id=None):
+        '''
+        This endpoint allows the client to retrieve a list of articles.
+        "Articles" are all the posts that users create on DEV that typically
+        show up in the feed. They can be a blog post, a discussion question,
+        a help thread etc. but is referred to as article within the code.
+
+        By default it will return featured, published articles ordered by descending popularity.
+
+        Each page will contain 30 articles.
+
+        Responses, according to the combination of params, are cached for 24 hours.
+
+        learn more at:
+
+        https://docs.dev.to/api/#tag/articles
+        '''
+        if id:
+            request = requests.get(url=self._get_url(path=f'/articles/{id}'))
+        else:
+            request = requests.get(url=self._get_url(path=f'/articles/'))
+        return request.json()
